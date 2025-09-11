f85f14f1263d372f8eef9bab2810d0eced891ee7
Aryan Khokhar <aryankhokhar022@gmail.com>
2025-09-11 18:50:23 +0530

    Login Logout controllers

--- Backend/src/controllers/auth.controllers.js
+++ Backend/src/controllers/auth.controllers.js
@@ -57,17 +57,42 @@ module.exports.login = async (req, res) => {
     try{
         const user = await User.findOne({email});
         if(!user){
-            return res.status(400).json({message:"User not found"});
+            return res.status(400).json({message:"Invalid credentials"});
         }
 
+        const isPasswordCorrect = await bcrypt.compare(password , user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+
+        generateToken(user._id , res);
+
+
+        return res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            email:user.email,
+            profilePic:user.profilePic,
+        });
+
+
+
     }catch(error){
-        console.log("Error in singup controller :" , error.message);
+        console.log("Error in login controller :" , error.message);
         return res.status(500).json({message:"Internal server error"});
     }
 }
 
 
-module.exports.logout = async (req, res) => {}
+module.exports.logout = async (req, res) => {
+    try{
+        res.cookie("jwt","",{maxAge:0});
+        return res.status(200).json({message:"Logged out successfully"});
+    }catch(err){
+        console.log("Error in logout controller :" , err.message);
+        return res.status(500).json({message:"Internal server error"});
+    }
+}
 
 
 module.exports.updateProfile = async (req, res) => {}
